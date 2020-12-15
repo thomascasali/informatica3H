4e6118528cab43471471768b6655472b05f1cd78
Thomas Casali <casali.thomas@gmail.com>
2020-12-15 09:52:22 +0100

    esercizi vettori 15/12/2020

---
+++ InvertiVettore/InvertiVettore/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvertiVettore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int dim,i,k;
+            var rnd = new Random();
+
+            Console.Write("Inserisci la dimensione del vettore:");
+            dim = int.Parse(Console.ReadLine());
+            var vettore = new int[dim];
+            var vettoreInverso = new int[dim];
+
+            for (i = 0; i < dim; i++)
+            {
+                //estrazione casuale
+                vettore[i] = rnd.Next(100);
+            }
+
+            for (i = 0; i < dim; i++)
+            {
+                //inverto il vettore
+                vettoreInverso[i] = vettore[dim-i-1];
+            }
+
+            //stampa dei vettori
+            for (i = 0; i < dim; i++)
+            {
+                Console.Write(vettore[i] + ", "); 
+            }
+            Console.WriteLine("\n");
+            for (i = 0; i < dim; i++)
+            {
+                Console.Write(vettoreInverso[i] + ", ");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ RicercaVettoreDisordinato/RicercaVettoreDisordinato/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RicercaVettoreDisordinato
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int dimensione;
+            int numDaRicercare, i;
+            bool trovato = false;
+            var rnd = new Random();
+            do
+            {
+                Console.WriteLine("Inserisci la dimensione del vettore:");
+                dimensione = int.Parse(Console.ReadLine());
+            } while (dimensione < 10 || dimensione > 100);
+            var vettore = new int[dimensione]; //istanzio oggetto vettore
+
+            for (i = 0; i < dimensione; i++)
+            {
+                vettore[i] = rnd.Next(1, 1001); //riempio il vettore di numeri da 1 a 1000
+                Console.Write(vettore[i] + ", ");
+            }
+
+            do
+            {
+                Console.Write("\nInserisci un numero da ricercare all'interno del vettore: ");
+                numDaRicercare = int.Parse(Console.ReadLine());
+
+                for (i = 0; i < dimensione; i++)
+                {
+                    if (vettore[i] == numDaRicercare) break;
+                }
+
+                if (i == dimensione)
+                {
+                    Console.WriteLine("Il numero inserito non è presente nel vettore");
+                }
+                else
+                {
+                    Console.WriteLine($"Il numero inserito è presente nel vettore nella posizione {i}");
+                    trovato = true;
+                }
+            } while (trovato == false);
+            Console.ReadKey();
+        }
+    }
+}
