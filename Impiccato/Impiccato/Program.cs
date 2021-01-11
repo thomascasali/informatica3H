bf8c53cdabb5b899f2bcbfb717bc3c267b3dacb2
Thomas Casali <casali.thomas@gmail.com>
2021-01-11 16:49:58 +0100

    impiccato

---
+++ Impiccato/Impiccato/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Impiccato
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var paroleDaIndovinare = new string[] {"difficilissima","ciao"};
+        }
+    }
+}
