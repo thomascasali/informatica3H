0300458270cb366c38839f46ee7087fa800c0617
Thomas Casali <casali.thomas@gmail.com>
2021-03-23 09:48:08 +0100

    gioco del 15 in Windows Forms

---
+++ Gioco del 15 in WF/Gioco del 15 in WF/Form1.Designer.cs
@@ -0,0 +1,258 @@
+﻿
+namespace Gioco_del_15_in_WF
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn00 = new System.Windows.Forms.Button();
+            this.btn01 = new System.Windows.Forms.Button();
+            this.btn02 = new System.Windows.Forms.Button();
+            this.btn03 = new System.Windows.Forms.Button();
+            this.btn10 = new System.Windows.Forms.Button();
+            this.btn11 = new System.Windows.Forms.Button();
+            this.btn12 = new System.Windows.Forms.Button();
+            this.btn13 = new System.Windows.Forms.Button();
+            this.btn20 = new System.Windows.Forms.Button();
+            this.btn21 = new System.Windows.Forms.Button();
+            this.btn22 = new System.Windows.Forms.Button();
+            this.btn23 = new System.Windows.Forms.Button();
+            this.btn30 = new System.Windows.Forms.Button();
+            this.btn31 = new System.Windows.Forms.Button();
+            this.btn32 = new System.Windows.Forms.Button();
+            this.btn33 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn00
+            // 
+            this.btn00.Font = new System.Drawing.Font("Microsoft YaHei", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn00.Location = new System.Drawing.Point(22, 12);
+            this.btn00.Name = "btn00";
+            this.btn00.Size = new System.Drawing.Size(75, 75);
+            this.btn00.TabIndex = 0;
+            this.btn00.UseVisualStyleBackColor = true;
+            this.btn00.Click += new System.EventHandler(this.Btn_Click);
+            // 
+            // btn01
+            // 
+            this.btn01.Font = new System.Drawing.Font("Microsoft YaHei", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn01.Location = new System.Drawing.Point(103, 12);
+            this.btn01.Name = "btn01";
+            this.btn01.Size = new System.Drawing.Size(75, 75);
+            this.btn01.TabIndex = 1;
+            this.btn01.UseVisualStyleBackColor = true;
+            this.btn01.Click += new System.EventHandler(this.Btn_Click);
+            // 
+            // btn02
+            // 
+            this.btn02.Font = new System.Drawing.Font("Microsoft YaHei", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn02.Location = new System.Drawing.Point(184, 12);
+            this.btn02.Name = "btn02";
+            this.btn02.Size = new System.Drawing.Size(75, 75);
+            this.btn02.TabIndex = 2;
+            this.btn02.UseVisualStyleBackColor = true;
+            this.btn02.Click += new System.EventHandler(this.Btn_Click);
+            // 
+            // btn03
+            // 
+            this.btn03.Font = new System.Drawing.Font("Microsoft YaHei", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn03.Location = new System.Drawing.Point(265, 12);
+            this.btn03.Name = "btn03";
+            this.btn03.Size = new System.Drawing.Size(75, 75);
+            this.btn03.TabIndex = 3;
+            this.btn03.UseVisualStyleBackColor = true;
+            this.btn03.Click += new System.EventHandler(this.Btn_Click);
+            // 
+            // btn10
+            // 
+            this.btn10.Font = new System.Drawing.Font("Microsoft YaHei", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn10.Location = new System.Drawing.Point(22, 93);
+            this.btn10.Name = "btn10";
+            this.btn10.Size = new System.Drawing.Size(75, 75);
+            this.btn10.TabIndex = 4;
+            this.btn10.UseVisualStyleBackColor = true;
+            this.btn10.Click += new System.EventHandler(this.Btn_Click);
+            // 
+            // btn11
+            // 
+            this.btn11.Font = new System.Drawing.Font("Microsoft YaHei", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn11.Location = new System.Drawing.Point(103, 93);
+            this.btn11.Name = "btn11";
+            this.btn11.Size = new System.Drawing.Size(75, 75);
+            this.btn11.TabIndex = 5;
+            this.btn11.UseVisualStyleBackColor = true;
+            this.btn11.Click += new System.EventHandler(this.Btn_Click);
+            // 
+            // btn12
+            // 
+            this.btn12.Font = new System.Drawing.Font("Microsoft YaHei", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn12.Location = new System.Drawing.Point(184, 93);
+            this.btn12.Name = "btn12";
+            this.btn12.Size = new System.Drawing.Size(75, 75);
+            this.btn12.TabIndex = 6;
+            this.btn12.UseVisualStyleBackColor = true;
+            this.btn12.Click += new System.EventHandler(this.Btn_Click);
+            // 
+            // btn13
+            // 
+            this.btn13.Font = new System.Drawing.Font("Microsoft YaHei", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn13.Location = new System.Drawing.Point(265, 93);
+            this.btn13.Name = "btn13";
+            this.btn13.Size = new System.Drawing.Size(75, 75);
+            this.btn13.TabIndex = 7;
+            this.btn13.UseVisualStyleBackColor = true;
+            this.btn13.Click += new System.EventHandler(this.Btn_Click);
+            // 
+            // btn20
+            // 
+            this.btn20.Font = new System.Drawing.Font("Microsoft YaHei", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn20.Location = new System.Drawing.Point(22, 174);
+            this.btn20.Name = "btn20";
+            this.btn20.Size = new System.Drawing.Size(75, 75);
+            this.btn20.TabIndex = 8;
+            this.btn20.UseVisualStyleBackColor = true;
+            this.btn20.Click += new System.EventHandler(this.Btn_Click);
+            // 
+            // btn21
+            // 
+            this.btn21.Font = new System.Drawing.Font("Microsoft YaHei", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn21.Location = new System.Drawing.Point(103, 174);
+            this.btn21.Name = "btn21";
+            this.btn21.Size = new System.Drawing.Size(75, 75);
+            this.btn21.TabIndex = 9;
+            this.btn21.UseVisualStyleBackColor = true;
+            this.btn21.Click += new System.EventHandler(this.Btn_Click);
+            // 
+            // btn22
+            // 
+            this.btn22.Font = new System.Drawing.Font("Microsoft YaHei", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn22.Location = new System.Drawing.Point(184, 174);
+            this.btn22.Name = "btn22";
+            this.btn22.Size = new System.Drawing.Size(75, 75);
+            this.btn22.TabIndex = 10;
+            this.btn22.UseVisualStyleBackColor = true;
+            this.btn22.Click += new System.EventHandler(this.Btn_Click);
+            // 
+            // btn23
+            // 
+            this.btn23.Font = new System.Drawing.Font("Microsoft YaHei", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn23.Location = new System.Drawing.Point(265, 174);
+            this.btn23.Name = "btn23";
+            this.btn23.Size = new System.Drawing.Size(75, 75);
+            this.btn23.TabIndex = 11;
+            this.btn23.UseVisualStyleBackColor = true;
+            this.btn23.Click += new System.EventHandler(this.Btn_Click);
+            // 
+            // btn30
+            // 
+            this.btn30.Font = new System.Drawing.Font("Microsoft YaHei", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn30.Location = new System.Drawing.Point(22, 255);
+            this.btn30.Name = "btn30";
+            this.btn30.Size = new System.Drawing.Size(75, 75);
+            this.btn30.TabIndex = 12;
+            this.btn30.UseVisualStyleBackColor = true;
+            this.btn30.Click += new System.EventHandler(this.Btn_Click);
+            // 
+            // btn31
+            // 
+            this.btn31.Font = new System.Drawing.Font("Microsoft YaHei", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn31.Location = new System.Drawing.Point(103, 255);
+            this.btn31.Name = "btn31";
+            this.btn31.Size = new System.Drawing.Size(75, 75);
+            this.btn31.TabIndex = 13;
+            this.btn31.UseVisualStyleBackColor = true;
+            this.btn31.Click += new System.EventHandler(this.Btn_Click);
+            // 
+            // btn32
+            // 
+            this.btn32.Font = new System.Drawing.Font("Microsoft YaHei", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn32.Location = new System.Drawing.Point(184, 255);
+            this.btn32.Name = "btn32";
+            this.btn32.Size = new System.Drawing.Size(75, 75);
+            this.btn32.TabIndex = 14;
+            this.btn32.UseVisualStyleBackColor = true;
+            this.btn32.Click += new System.EventHandler(this.Btn_Click);
+            // 
+            // btn33
+            // 
+            this.btn33.Font = new System.Drawing.Font("Microsoft YaHei", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn33.Location = new System.Drawing.Point(265, 255);
+            this.btn33.Name = "btn33";
+            this.btn33.Size = new System.Drawing.Size(75, 75);
+            this.btn33.TabIndex = 15;
+            this.btn33.UseVisualStyleBackColor = true;
+            this.btn33.Click += new System.EventHandler(this.Btn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(362, 353);
+            this.Controls.Add(this.btn33);
+            this.Controls.Add(this.btn32);
+            this.Controls.Add(this.btn31);
+            this.Controls.Add(this.btn30);
+            this.Controls.Add(this.btn23);
+            this.Controls.Add(this.btn22);
+            this.Controls.Add(this.btn21);
+            this.Controls.Add(this.btn20);
+            this.Controls.Add(this.btn13);
+            this.Controls.Add(this.btn12);
+            this.Controls.Add(this.btn11);
+            this.Controls.Add(this.btn10);
+            this.Controls.Add(this.btn03);
+            this.Controls.Add(this.btn02);
+            this.Controls.Add(this.btn01);
+            this.Controls.Add(this.btn00);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn00;
+        private System.Windows.Forms.Button btn01;
+        private System.Windows.Forms.Button btn02;
+        private System.Windows.Forms.Button btn03;
+        private System.Windows.Forms.Button btn10;
+        private System.Windows.Forms.Button btn11;
+        private System.Windows.Forms.Button btn12;
+        private System.Windows.Forms.Button btn13;
+        private System.Windows.Forms.Button btn20;
+        private System.Windows.Forms.Button btn21;
+        private System.Windows.Forms.Button btn22;
+        private System.Windows.Forms.Button btn23;
+        private System.Windows.Forms.Button btn30;
+        private System.Windows.Forms.Button btn31;
+        private System.Windows.Forms.Button btn32;
+        private System.Windows.Forms.Button btn33;
+    }
+}
+
---
+++ Gioco del 15 in WF/Gioco del 15 in WF/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Gioco_del_15_in_WF
+{
+    public partial class Form1 : Form
+    {
+        static int[,] gioco15 = new int[4, 4]; //definisco la matrice di gioco
+        static Random rnd = new Random();
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int x, y;
+            for (int i = 1; i < 16; i++)
+            {
+                do
+                {
+                    x = rnd.Next(4); //estrae un numero casuale da 0 a 3 che rappresenta la riga estratta
+                    y = rnd.Next(4); //estrae un numero casuale da 0 a 3 che rappresenta la colonna estratta
+                } while (gioco15[x, y] != 0);
+                gioco15[x, y] = i;
+            }
+            StampaCampo();
+        }
+
+        private void StampaCampo() {
+            if (gioco15[0, 0] != 0) btn00.Text = gioco15[0, 0].ToString(); else btn00.Text = "";
+            if (gioco15[0, 1] != 0) btn01.Text = gioco15[0, 1].ToString(); else btn01.Text = "";
+            if (gioco15[0, 2] != 0) btn02.Text = gioco15[0, 2].ToString(); else btn02.Text = "";
+            if (gioco15[0, 3] != 0) btn03.Text = gioco15[0, 3].ToString(); else btn03.Text = "";
+            if (gioco15[1, 0] != 0) btn10.Text = gioco15[1, 0].ToString(); else btn10.Text = "";
+            if (gioco15[1, 1] != 0) btn11.Text = gioco15[1, 1].ToString(); else btn11.Text = "";
+            if (gioco15[1, 2] != 0) btn12.Text = gioco15[1, 2].ToString(); else btn12.Text = "";
+            if (gioco15[1, 3] != 0) btn13.Text = gioco15[1, 3].ToString(); else btn13.Text = "";
+            if (gioco15[2, 0] != 0) btn20.Text = gioco15[2, 0].ToString(); else btn20.Text = "";
+            if (gioco15[2, 1] != 0) btn21.Text = gioco15[2, 1].ToString(); else btn21.Text = "";
+            if (gioco15[2, 2] != 0) btn22.Text = gioco15[2, 2].ToString(); else btn22.Text = "";
+            if (gioco15[2, 3] != 0) btn23.Text = gioco15[2, 3].ToString(); else btn23.Text = "";
+            if (gioco15[3, 0] != 0) btn30.Text = gioco15[3, 0].ToString(); else btn30.Text = "";
+            if (gioco15[3, 1] != 0) btn31.Text = gioco15[3, 1].ToString(); else btn31.Text = "";
+            if (gioco15[3, 2] != 0) btn32.Text = gioco15[3, 2].ToString(); else btn32.Text = "";
+            if (gioco15[3, 3] != 0) btn33.Text = gioco15[3, 3].ToString(); else btn33.Text = "";
+        }
+
+        private void Btn_Click(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            //recupero la posizione della matrice cliccata dal nome del bottone
+            int x = int.Parse(b.Name[3].ToString()); 
+            int y= int.Parse(b.Name[4].ToString());
+            //oppure si ottiene la stessa cosa utilizzando il codice ASCII del carattere
+            x = b.Name[3] - 48;
+            y = b.Name[4] - 48;
+            //MessageBox.Show(gioco15[x, y].ToString());
+
+            //adesso cerco lo 0 da scambiare nei dintorni della posizione x,y
+            for (int i = x - 1; i < x + 2; i++) { 
+                for (int j = y - 1; j < y + 2; j++)
+                {
+                    //controllo che gli indici i e j siano "buoni" per la matrice
+                    if(i >= 0 && i <= 3 && j >= 0 && j <= 3)
+                    {
+                        //verifico se c'è lo 0 e sono nella stessa riga o nella stessa colonna
+                        if (gioco15[i, j] == 0 && (i==x||j==y))
+                        {
+                            //ho trovato lo 0 e scambio i,j con x,y
+                            gioco15[i, j] = gioco15[x, y];
+                            gioco15[x, y] = 0;
+                            break;
+                        }
+                    }
+                }
+            }
+            //ristampo il campo
+            StampaCampo();
+        }
+
+    }
+}
