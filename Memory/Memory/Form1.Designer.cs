8d6fe87cb467bf50a3cbdbf7e4defb7f1aa2de17
Thomas Casali <casali.thomas@gmail.com>
2021-03-29 13:38:39 +0200

    intro Memory

---
+++ Memory/Memory/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿
+namespace Memory
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pic00 = new System.Windows.Forms.PictureBox();
+            this.pic01 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pan = new System.Windows.Forms.Panel();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pic00)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic01)).BeginInit();
+            this.pan.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pic00
+            // 
+            this.pic00.Location = new System.Drawing.Point(97, 83);
+            this.pic00.Name = "pic00";
+            this.pic00.Size = new System.Drawing.Size(146, 142);
+            this.pic00.TabIndex = 0;
+            this.pic00.TabStop = false;
+            // 
+            // pic01
+            // 
+            this.pic01.Location = new System.Drawing.Point(268, 83);
+            this.pic01.Name = "pic01";
+            this.pic01.Size = new System.Drawing.Size(146, 142);
+            this.pic01.TabIndex = 1;
+            this.pic01.TabStop = false;
+            this.pic01.Click += new System.EventHandler(this.pic01_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(104, 257);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(164, 49);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "nascondi";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(104, 312);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(164, 49);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "scopri";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pan
+            // 
+            this.pan.Controls.Add(this.button3);
+            this.pan.Controls.Add(this.radioButton2);
+            this.pan.Controls.Add(this.radioButton1);
+            this.pan.Location = new System.Drawing.Point(461, 83);
+            this.pan.Name = "pan";
+            this.pan.Size = new System.Drawing.Size(230, 162);
+            this.pan.TabIndex = 6;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(23, 47);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(67, 21);
+            this.radioButton2.TabIndex = 6;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "medio";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(23, 20);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(62, 21);
+            this.radioButton1.TabIndex = 5;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "facile";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(29, 90);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(123, 42);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "START!";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pan);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pic01);
+            this.Controls.Add(this.pic00);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pic00)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic01)).EndInit();
+            this.pan.ResumeLayout(false);
+            this.pan.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pic00;
+        private System.Windows.Forms.PictureBox pic01;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel pan;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+    }
+}
+
---
+++ Memory/Memory/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Memory
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pic00.Image = Properties.Resources.gattino;
+            pic00.SizeMode = PictureBoxSizeMode.StretchImage;
+            pic01.Image = Properties.Resources.ben10;
+            pic01.SizeMode = PictureBoxSizeMode.StretchImage;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pic00.Visible = false;
+        }
+
+        private void pic01_Click(object sender, EventArgs e)
+        {
+            pic01.Visible = false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            pic00.Visible = true;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            pan.Visible = false;
+        }
+    }
+}
