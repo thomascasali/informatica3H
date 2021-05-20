4a4b5f10edea612e7c9e060b84da3eb23a6379e7
Thomas Casali <casali.thomas@gmail.com>
2021-05-20 08:53:49 +0200

    Esempio di gestione celle in evidenza in datagrid

---
+++ EvidenzaCellaDatagrid/EvidenzaCellaDatagrid/Form1.Designer.cs
@@ -0,0 +1,67 @@
+﻿
+namespace EvidenzaCellaDatagrid
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dtgrid = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dtgrid
+            // 
+            this.dtgrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgrid.Location = new System.Drawing.Point(42, 41);
+            this.dtgrid.Name = "dtgrid";
+            this.dtgrid.RowHeadersWidth = 51;
+            this.dtgrid.RowTemplate.Height = 24;
+            this.dtgrid.Size = new System.Drawing.Size(359, 344);
+            this.dtgrid.TabIndex = 0;
+            this.dtgrid.CellMouseEnter += new System.Windows.Forms.DataGridViewCellEventHandler(this.dtgrid_CellMouseEnter);
+            this.dtgrid.CellMouseLeave += new System.Windows.Forms.DataGridViewCellEventHandler(this.dtgrid_CellMouseLeave);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dtgrid);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dtgrid)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dtgrid;
+    }
+}
+
---
+++ EvidenzaCellaDatagrid/EvidenzaCellaDatagrid/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EvidenzaCellaDatagrid
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dtgrid.Width = 300;
+            dtgrid.Height = 300;
+            dtgrid.RowCount = 10;
+            dtgrid.ColumnCount = 10;
+            dtgrid.ColumnHeadersVisible = false;
+            dtgrid.RowHeadersVisible = false;
+            foreach (DataGridViewColumn cs in dtgrid.Columns)
+            {
+                cs.Width = (dtgrid.Width-1) / 10;
+            }
+            foreach (DataGridViewRow cs in dtgrid.Rows)
+            {
+                cs.Height = (dtgrid.Height-1) / 10;
+            }
+
+        }
+
+        private void dtgrid_CellMouseEnter(object sender, DataGridViewCellEventArgs e)
+        {
+            dtgrid.Rows[e.RowIndex].Cells[e.ColumnIndex].Style.BackColor = Color.Blue;
+            try
+            {
+                dtgrid.Rows[e.RowIndex].Cells[e.ColumnIndex + 1].Style.BackColor = Color.Blue;
+            }
+            catch { }
+        }
+
+        private void dtgrid_CellMouseLeave(object sender, DataGridViewCellEventArgs e)
+        {
+            dtgrid.Rows[e.RowIndex].Cells[e.ColumnIndex].Style.BackColor = Color.White;
+            try
+            {
+                dtgrid.Rows[e.RowIndex].Cells[e.ColumnIndex + 1].Style.BackColor = Color.White;
+            }
+            catch { }
+        }
+    }
+}
